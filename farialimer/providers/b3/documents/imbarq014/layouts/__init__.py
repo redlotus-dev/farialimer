0dd58183a22b8921fb4915bbf59e3bf5e65acff6
Leandro Meili <leandro.meili@gmail.com>
2023-02-11 16:52:53 -0300

    fix import

--- farialimer/providers/b3/documents/imbarq014/layouts/__init__.py
+++ farialimer/providers/b3/documents/imbarq014/layouts/__init__.py
@@ -4,14 +4,14 @@ from farialimer.providers.b3.documents.imbarq014.layouts.layout_00 import (
     layout as layout_00,
 )
 from farialimer.providers.b3.documents.imbarq014.layouts.layout_18 import (
-    layout as layout_18,
+    LAYOUT as layout_18,
 )
 from farialimer.providers.b3.documents.imbarq014.layouts.layout_19 import (
-    layout as layout_19,
+    LAYOUT as layout_19,
 )
 from farialimer.providers.b3.documents.imbarq014.layouts.layout_31 import (
-    layout as layout_31,
+    LAYOUT as layout_31,
 )
 from farialimer.providers.b3.documents.imbarq014.layouts.layout_99 import (
-    layout as layout_99,
+    LAYOUT as layout_99,
 )
