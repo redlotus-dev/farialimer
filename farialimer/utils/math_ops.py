a51b79ed2181ba1d36515e51fc5815f7d76fbe22
Leandro Meili <lmeilibr@users.noreply.github.com>
2023-02-14 23:17:40 -0300

    Lm/yml parser (#14)
    * refact with yaml parser
    
    * add PyYaml
    
    * swap workflow
    
    * reformat
    
    * update black

---
+++ farialimer/utils/math_ops.py
@@ -0,0 +1,11 @@
+"""Module for common and re-usable functions"""
+
+from typing import List
+
+from farialimer.specs.b3.imbarq014.layouts.layout_00 import Field
+
+
+def sum_layout_fields_size(layout: List[Field]):
+    """Given a layout, sum all the fields length"""
+    sum_size = sum((item.end - item.start + 1) for item in layout)
+    return sum_size
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,13 +1,13 @@
 """Test utils module"""
 
 from datetime import date
-from farialimer.utils import convert_ymd
+from farialimer.utils.converters import convert_yyyymmdd
 
 
-def test_convert_ymd():
+def test_convert_yyyymmdd():
     """Test year-month-date string conversion do datetime.date"""
     base = "20220526"
     expected = date(2022, 5, 26)
-    result = convert_ymd(base)
+    result = convert_yyyymmdd(base)
 
     assert result == expected
