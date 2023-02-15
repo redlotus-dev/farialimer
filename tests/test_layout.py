e8c12c677afab1f061b36497a619bd7cb7536e77
Leandro Meili <leandro.meili@gmail.com>
2023-02-14 23:44:23 -0300

    reformat test

--- tests/test_layout.py
+++ tests/test_layout.py
@@ -1,13 +1,12 @@
 """"Test Layout Properties"""
 
-
 from farialimer.utils.math_ops import sum_layout_fields_size
 from farialimer.parser import Parser
 
 
-
 def test_layout_size():
     """Test the Sum of layout field sizes"""
+
     parser = Parser()
     layout = parser.get_doc_parser("imbarq014")
     result = sum_layout_fields_size(layout)
