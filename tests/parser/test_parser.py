a40667f075f5380e2347562e53ed8826191518c6
Leandro Meili <leandro.meili@gmail.com>
2023-02-25 16:36:55 -0300

    finish imb006

--- tests/parser/test_parser.py
+++ tests/parser/test_parser.py
@@ -38,6 +38,10 @@ def test_get_filetype():
             "tests/specs/b3/samples/imbarq004/IMBARQ004_BV000272202204060000001001512113233.txt",
         ),
         (
+            "imbarq006",
+            "tests/specs/b3/samples/imbarq006/IMBARQ006_BV000272202204050000001000935195407.txt",
+        ),
+        (
             "imbarq014",
             "tests/specs/b3/samples/imbarq014/IMBARQ014_BV000272202205260000001000935071656.txt",
         ),
