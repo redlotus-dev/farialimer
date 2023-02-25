b09e9282eaf84687288dcb8f0616f367b8d1d52c
Leandro Meili <lmeilibr@users.noreply.github.com>
2023-02-25 00:47:10 -0300

    Lm/22 imbarq001 (#24)
    * add b3 public samples
    
    * add register 01
    
    * add register 03
    
    * add register 05
    
    * add register 06
    
    * add register 33
    
    * add register 07
    
    * add register 08
    
    * add register 09
    
    * add imbarq014 version
    
    * add imbarq001 register 10
    
    * add imbarq001 register 11
    
    * add imbarq001 register 36
    
    * add imbarq001 register 12
    
    * add imbarq001 register 13
    
    * add imbarq001 register 14
    
    * add imbarq001 register 15
    
    * add imbarq001 register 16
    
    * add imbarq001 register 17
    
    * add imbarq001 fix typos
    
    * add imbarq001 register 20
    
    * add imbarq001 register 21
    
    * add imbarq001 register 22
    
    * add imbarq001 register 23
    
    * add imbarq001 register 23
    
    * add imbarq001 register 25
    
    * add imbarq001 register 26
    
    * add imbarq001 register 27
    
    * add imbarq001 register 28
    
    * add imbarq001 register 29
    
    * add imbarq001 full yaml
    
    * cache yaml read tests

---
+++ tests/parser/test_b3_layout.py
@@ -0,0 +1,71 @@
+""""Test Layout Properties"""
+from functools import lru_cache
+
+import pytest
+
+from farialimer.parser.b3parser import B3Parser as Parser
+from farialimer.utils.math_ops import sum_layout_fields_size
+
+
+@pytest.mark.parametrize(
+    "layout, register",
+    [
+        ("imbarq001", "00"),
+        ("imbarq001", "01"),
+        ("imbarq001", "02"),
+        ("imbarq001", "03"),
+        ("imbarq001", "04"),
+        ("imbarq001", "05"),
+        ("imbarq001", "06"),
+        ("imbarq001", "07"),
+        ("imbarq001", "08"),
+        ("imbarq001", "09"),
+        ("imbarq001", "10"),
+        ("imbarq001", "11"),
+        ("imbarq001", "12"),
+        ("imbarq001", "13"),
+        ("imbarq001", "14"),
+        ("imbarq001", "15"),
+        ("imbarq001", "16"),
+        ("imbarq001", "17"),
+        ("imbarq001", "18"),
+        ("imbarq001", "19"),
+        ("imbarq001", "20"),
+        ("imbarq001", "21"),
+        ("imbarq001", "22"),
+        ("imbarq001", "23"),
+        ("imbarq001", "24"),
+        ("imbarq001", "25"),
+        ("imbarq001", "26"),
+        ("imbarq001", "27"),
+        ("imbarq001", "28"),
+        ("imbarq001", "29"),
+        ("imbarq001", "30"),
+        ("imbarq001", "31"),
+        ("imbarq001", "32"),
+        ("imbarq001", "34"),
+        ("imbarq001", "99"),
+        ("imbarq014", "00"),
+        ("imbarq014", "18"),
+        ("imbarq014", "19"),
+        ("imbarq014", "31"),
+        ("imbarq014", "99"),
+    ],
+)
+def test_layout_size(layout, register):
+    """Test the Sum of layout field sizes"""
+
+    layout = get_layout(layout)
+    result = sum_layout_fields_size(layout, register)
+    expected = 1000
+
+    assert result == expected
+
+
+@lru_cache(maxsize=None)
+def get_layout(layout):
+    """To avoid opening the spec file multiples times,
+    we use a cache to store the layout object"""
+    parser = Parser("b3")
+    layout = parser.get_doc_parser(layout)
+    return layout
